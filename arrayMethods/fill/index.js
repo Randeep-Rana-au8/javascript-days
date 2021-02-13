17b8ce7ec30fb584c98c1dc1bc395526d8dc9b1e
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-13 16:14:38 +0530

    Javascript array fill() method

---
+++ arrayMethods/fill/index.js
@@ -0,0 +1,8 @@
+var fruits = ["apple", "mango", "banana"];
+
+console.log(fruits.fill("guava"));
+// This method fills the elements of an array with the given value, if position is not specified then it will fill all the elments
+
+// console.log(fruits.fill("apple", 1));
+
+// console.log(fruits.fill("banana", 2));
